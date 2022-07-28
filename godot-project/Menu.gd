d6d3f600fcc72c2c5fd822ae631fc6d45101d34c
Yukihiro Saito <yukky.saito@gmail.com>
2022-07-28 23:25:08 +0900

    cosmetic change
    Signed-off-by: Yukihiro Saito <yukky.saito@gmail.com>

--- godot-project/Menu.gd
+++ godot-project/Menu.gd
@@ -13,7 +13,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("ui_select"):
 		visible = not visible
 #	pass
