8feb4f72408d1733c6fef52bb0e3cfaad37d7c76
Yukihito Saito <yukky.saito@gmail.com>
2023-03-15 01:19:20 +0900

    update vehicle model

--- godot-project/EgoVehicle.gd
+++ godot-project/EgoVehicle.gd
@@ -13,39 +13,36 @@ func _process(_delta):
 
 	# Indicators
 	if(vehicle_status.has_new()):
-		var right_light = get_node("EgoVehicleKinematicBody/VehicleBody/TurnSignalR")
-		var right_small_light = get_node("EgoVehicleKinematicBody/VehicleBody/FrontTurnSignalR")
+		var right_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/TurnSignalR")
 		if (vehicle_status.is_turn_on_right()):
 			right_light.turn_on()
-			right_small_light.turn_on()
 		else:
 			right_light.turn_off()
-			right_small_light.turn_off()
 
-		var left_light = get_node("EgoVehicleKinematicBody/VehicleBody/TurnSignalL")
-		var left_small_light = get_node("EgoVehicleKinematicBody/VehicleBody/FrontTurnSignalL")
+		var left_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/TurnSignalL")
 		if (vehicle_status.is_turn_on_left()):
 			left_light.turn_on()
-			left_small_light.turn_on()
 		else:
 			left_light.turn_off()
-			left_small_light.turn_off()
 		vehicle_status.set_old()
 
 
 
 func _on_NightModeCheckButton_toggled(button_pressed):
-	var head_light = get_node("EgoVehicleKinematicBody/VehicleBody/HeadLight")
-	var head_small_light = get_node("EgoVehicleKinematicBody/VehicleBody/HeadSmallLight")
-	var brake_light = get_node("EgoVehicleKinematicBody/VehicleBody/BrakeLight")
+	var head_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/HeadLight")
+	var head_small_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/HeadSmallLight")
+	var fog_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/FogLight")
+	var brake_light = get_node("EgoVehicleKinematicBody/VehicleBody/RootNode/BrakeLight")
 	var head_beam_light = get_node("EgoVehicleKinematicBody/HeadBeamLight")
 	if (button_pressed):
 		head_light.turn_on()
 		head_small_light.turn_on()
+		fog_light.turn_on()
 		brake_light.night_light_turn_on()
 		head_beam_light.turn_on()
 	else:
 		head_light.turn_off()
 		head_small_light.turn_off()
+		fog_light.turn_off()
 		brake_light.night_light_turn_off()
 		head_beam_light.turn_off()
