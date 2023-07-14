deba59d0db475ec1de554d45678c6f9f2e2ce4fe
Yukihito Saito <yukky.saito@gmail.com>
2023-07-15 00:20:52 +0900

    test
    Signed-off-by: Yukihito Saito <yukky.saito@gmail.com>

--- godot_rviz2/src/behavior_path.cpp
+++ godot_rviz2/src/behavior_path.cpp
@@ -34,6 +34,9 @@ void BehaviorPath::_bind_methods()
 Array BehaviorPath::get_triangle_strip_with_velocity(const float width)
 {
   Array triangle_strip_with_velocity;
+  Array aaa;
+  Array aaaa;
+  Array bbbb;  // remove
   PoolVector3Array triangle_points;
   const auto last_msg = get_last_msg();
   if (!last_msg) return triangle_strip_with_velocity;
