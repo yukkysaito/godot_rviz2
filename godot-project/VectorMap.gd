26e0a354e191279672d2147c69ec7b53787e066e
Yukihito Saito <yukky.saito@gmail.com>
2023-10-15 13:09:33 +0900

    add some lllw2

--- godot-project/VectorMap.gd
+++ godot-project/VectorMap.gd
@@ -10,15 +10,21 @@ func _process(_delta):
 		return
 	# Road Surface
 	var road_surface = get_node("RoadSurfaceMesh")
-	road_surface.visualize_mesh(vector_map.get_triangle_marker("road_lanelets"))
+	var road_surface_verts = PoolVector3Array()
+	road_surface_verts.append_array(vector_map.get_triangle_marker("road_lanelets"))
+	road_surface_verts.append_array(vector_map.get_triangle_marker("shoulder_road_lanelets"))
+	road_surface.visualize_mesh(road_surface_verts)
 	# Road Marker
 	var road_marker = get_node("RoadMarkerMesh")
 	var road_marker_verts = PoolVector3Array()
 	road_marker_verts.append_array(vector_map.get_triangle_marker("right_lane_bound"))
 	road_marker_verts.append_array(vector_map.get_triangle_marker("left_lane_bound"))
+	road_marker_verts.append_array(vector_map.get_triangle_marker("shoulder_right_lane_bound"))
+	road_marker_verts.append_array(vector_map.get_triangle_marker("shoulder_left_lane_bound"))
 	road_marker_verts.append_array(vector_map.get_triangle_marker("pedestrian_marking"))
 	road_marker_verts.append_array(vector_map.get_triangle_marker("stop_lines"))
 	road_marker_verts.append_array(vector_map.get_triangle_marker("intersection_area"))
+	road_marker_verts.append_array(vector_map.get_triangle_marker("parking_lots"))
 	road_marker.visualize_mesh(road_marker_verts)
 	# Traffic Light
 	var traffic_light = get_node("TrafficLightMesh")
