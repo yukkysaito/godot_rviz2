433942f3dc2fd01cfc79e314673c595f8119c754
KhalilSelyan <khalilselyan2@gmail.com>
2024-01-19 01:13:05 +0300

    feat: Addition of wheel rotation for smooth feel in 3d view

--- godot-project/EgoVehicle.gd
+++ godot-project/EgoVehicle.gd
@@ -2,14 +2,32 @@ extends Node3D
 
 var ego_pose = EgoPose.new()
 var vehicle_status = VehicleStatus.new()
+var velocity_report = VelocityReport.new()
+
+# Wheel nodes
+var wheel_back_r: Node3D
+var wheel_back_l: Node3D
+var wheel_front_r: Node3D
+var wheel_front_l: Node3D
 
 func _ready():
 	vehicle_status.subscribe("/vehicle/status/turn_indicators_status", false)
+	velocity_report.subscribe("/vehicle/status/velocity_status", false)
+	
+	wheel_back_r = get_node("EgoVehicleKinematicBody/VehicleBody3D/WheelBackR")
+	wheel_back_l = get_node("EgoVehicleKinematicBody/VehicleBody3D/WheelBackL")
+	wheel_front_r = get_node("EgoVehicleKinematicBody/VehicleBody3D/WheelFrontR")
+	wheel_front_l = get_node("EgoVehicleKinematicBody/VehicleBody3D/WheelFrontL")
+
 
 func _process(_delta):
 	# Ego pose
 	set_position(ego_pose.get_ego_position())
 	set_rotation(ego_pose.get_ego_rotation())
+	
+	if(velocity_report.has_new()):
+		rotate_wheels(_delta)
+		velocity_report.set_old()
 
 	# Indicators
 	if(vehicle_status.has_new()):
@@ -28,6 +46,17 @@ func _process(_delta):
 		else:
 			left_light.turn_off()
 		vehicle_status.set_old()
+		
+# Rotate the wheels based on the current speed
+func rotate_wheels(delta):
+	var current_speed  = velocity_report.get_velocity()
+	var rotation_amount = current_speed * delta * 2 * PI * 0.25  # Adjust this formula as needed
+
+	wheel_back_r.rotate_x(rotation_amount)
+	wheel_back_l.rotate_x(rotation_amount)
+	wheel_front_r.rotate_x(rotation_amount)
+	wheel_front_l.rotate_x(rotation_amount)
+
 
 func _on_night_mode_check_button_toggled(toggled_on):
 	var head_light = get_node("EgoVehicleKinematicBody/VehicleBody3D/HeadLight")
