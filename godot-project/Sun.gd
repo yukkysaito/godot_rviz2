bca1f9845d3d5202632bca1929a0963670ee5981
Yukihito Saito <yukky.saito@gmail.com>
2024-01-29 00:36:46 +0900

    cosmetic change
    Signed-off-by: Yukihito Saito <yukky.saito@gmail.com>

--- godot-project/Sun.gd
+++ godot-project/Sun.gd
@@ -1,16 +1,5 @@
 extends DirectionalLight3D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_night_mode_check_button_toggled(toggled_on):
 	if (toggled_on):
 		visible = false
