dad2592d8a87f1ebf75c95739559098a8a483936
Yukihito Saito <yukky.saito@gmail.com>
2024-02-10 23:13:31 +0900

    update obstacle segmentation
    Signed-off-by: Yukihito Saito <yukky.saito@gmail.com>

--- godot-project/ObstacleSegmentationMesh.gd
+++ godot-project/ObstacleSegmentationMesh.gd
@@ -1,11 +1,15 @@
 extends MeshInstance3D
 
 var pointcloud = PointCloud.new()
+var sum_time = 0.0
+var transparency_speed = 4.0
+var cycle_time = 3.0
 
 func _ready():
 	pointcloud.subscribe("/perception/obstacle_segmentation/pointcloud", false)
 
-func _process(_delta):
+func _process(delta):
+	sum_time += delta
 	if !pointcloud.has_new():
 		return
 
@@ -22,6 +26,13 @@ func _process(_delta):
 
 	arr[Mesh.ARRAY_VERTEX] = verts
 #	arr[Mesh.ARRAY_TEX_UV] = uvs
+
+	if sum_time * transparency_speed < 2 * PI:
+		transparency = (cos(sum_time * transparency_speed) + 1.0) * 0.5
+	if (cycle_time < sum_time):
+		sum_time = 0.0
+
+
 	if !verts.is_empty():
 		mesh.clear_surfaces()
 		mesh.add_surface_from_arrays(Mesh.PRIMITIVE_POINTS, arr)
